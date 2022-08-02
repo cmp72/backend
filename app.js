2024b4bc7bb5f7946424321c69f25dffd599ed41
cmp72 <cmp-72@hotmail.com>
2022-08-02 19:45:14 +0200

    cambios3

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require ('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
+const { request } = require('http');
 
 var app = express();
 
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel= require('./../../models/usuariosModel');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,5 +9,26 @@ router.get('/', function(req, res, next) {
     layout: 'admin/layout'
   });
 });
+
+router.post('/', async (req, res, next) => {
+  try{
+    var usuario= req.body.usuario; //Captura el nombre del usuario
+    var password= req.body.password; //Captura el password del usuario
+
+    var data= await usuariosModel.getUser(usuario, password);
+  
+  if (data !=undefined){
+    res.redirect('/admin/novedades');
+
+  } else {
+    res.render ('admin/login', {
+      layout: 'admin/layout',
+      error: true
+    });
+  }
+} catch (error) {
+console.log(error);
+}
+})
 
 module.exports = router;
