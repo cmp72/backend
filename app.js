311f51feb3fea66b33e95fd6e61ecf6a24e98227
cmp72 <cmp-72@hotmail.com>
2022-08-02 20:23:25 +0200

    admin

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require ('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
+var adminRouter = require ('./routes/admin/novedades');
 const { request } = require('http');
 
 var app = express();
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use ('/admin/login', loginRouter);
+app.use ('/admin/novedades', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
