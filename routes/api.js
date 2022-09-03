32006e86bee2f7d8a6b3a370cb4902a46e53d74f
cmp72 <cmp-72@hotmail.com>
2022-09-03 07:23:17 +0200

    cambios 4

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express= require ('express');
 var router= express.Router();
 var novedadesModel= require('./../models/novedadesModel');
 var cloudinary= require ('cloudinary').v2;
+var nodemailer= require('nodemailer');
 
 router.get('/novedades', async function(req, res, next){
     let novedades = await novedadesModel.getNovedades();
@@ -27,5 +28,28 @@ router.get('/novedades', async function(req, res, next){
         }
     });
     res.json(novedades);
+});
+
+router.post( '/contacto', async (req, res) =>{
+    const mail= {
+        to: 'cmp-72@hotmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contacto a través de la web y quiere más información a este correo: ${req.body.email}
+        <br> Su teléfono es: ${req.body.telefono} `
+    }
+    const transport= nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        aut:{
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    }); //Cierra transport 
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error:false,
+        message: 'Mensaje enviado'
+    });
 });
 module.exports= router;
