cec44470bdfe166fd178367d14507132807d2d44
cmp72 <cmp-72@hotmail.com>
2022-09-05 21:28:54 +0200

    cambios 22

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var novedadesModel= require('./../models/novedadesModel');
 var cloudinary= require ('cloudinary').v2;
 var nodemailer= require('nodemailer');
 
-router.get('/novedades', async function(req, res, next){
+router.get('/novedades', async function (req, res, next){
     let novedades = await novedadesModel.getNovedades();
     
     novedades= novedades.map(novedades => {
@@ -30,13 +30,13 @@ router.get('/novedades', async function(req, res, next){
     res.json(novedades);
 });
 
-router.post( '/contacto', async (req, res) =>{
+router.post('/contacto', async (req, res) =>{
     const mail= {
         to: 'cmp-72@hotmail.com',
         subject: 'Contacto web',
         html: `${req.body.nombre} se contacto a través de la web y quiere más información a este correo: 
         ${req.body.email}
-        <br> Su teléfono es: ${req.body.telefono} `
+        <br> Su teléfono es: ${req.body.telefono}`
     }
     const transport= nodemailer.createTransport({
         host: process.env.SMTP_HOST,
